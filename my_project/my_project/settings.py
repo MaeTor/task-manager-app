1ad485f839f62ff975d393eaea311cb084920b03
Maetor <mael.torregrosa@etu.univ-amu.fr>
2026-03-17 23:18:03 +0100

    add my own host in ALLOWED_HOSTS to be able to access to the API

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y^ikg(q^^r@+1mhfj^te)3e*i0d^rffl3ahr7j$ww$&8i(%=w$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
